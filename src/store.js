7147a35a7324bc5be9943e2f42cc0f6356e4eecf
Lev Enders <leveders@mail.ru>
2024-09-19 20:34:55 +0300

    feat: add actions

--- src/store.js
+++ src/store.js
@@ -1,5 +1,3 @@
-import { generateCode } from './utils';
-
 /**
  * Хранилище состояния приложения
  */
@@ -40,47 +38,27 @@ class Store {
     for (const listener of this.listeners) listener();
   }
 
-  /**
-   * Добавление новой записи
-   */
-  addItem() {
-    this.setState({
-      ...this.state,
-      list: [...this.state.list, { code: generateCode(), title: 'Новая запись' }],
-    });
+  addToCart(code) {
+    const { cart, list } = this.state;
+    const existingItem = cart.find(item => item.code === code);
+    const item = list.find(item => item.code === code);
+
+    if (existingItem) {
+      existingItem.quantity += 1;
+      this.setState({ ...this.state, cart });
+      return;
+    }
+
+    if (item) {
+      cart.push({ ...item, quantity: 1 });
+      this.setState({ ...this.state, cart });
+    }
   }
 
-  /**
-   * Удаление записи по коду
-   * @param code
-   */
-  deleteItem(code) {
+  removeFromCart(code) {
     this.setState({
       ...this.state,
-      // Новый список, в котором не будет удаляемой записи
-      list: this.state.list.filter(item => item.code !== code),
-    });
-  }
-
-  /**
-   * Выделение записи по коду
-   * @param code
-   */
-  selectItem(code) {
-    this.setState({
-      ...this.state,
-      list: this.state.list.map(item => {
-        if (item.code === code) {
-          // Смена выделения и подсчёт
-          return {
-            ...item,
-            selected: !item.selected,
-            count: item.selected ? item.count : item.count + 1 || 1,
-          };
-        }
-        // Сброс выделения если выделена
-        return item.selected ? { ...item, selected: false } : item;
-      }),
+      cart: this.state.cart.filter(item => item.code !== code),
     });
   }
 }
