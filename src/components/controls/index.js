264994398e672b810dfe4d9968d9ea5a1f79e66a
Lev Enders <leveders@mail.ru>
2024-09-19 22:16:38 +0300

    fix: change three of controls component

--- src/components/controls/index.js
+++ src/components/controls/index.js
@@ -1,20 +1,33 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { plural } from '../../utils';
 import './style.css';
 
-function Controls({ onAdd, buttonLabel, title }) {
+function Controls({ buttonHandler, totalPrice, totalItems, buttonLabel }) {
   return (
     <div className="Controls">
-      <div>{title}</div>
-      <button onClick={() => onAdd()}>{buttonLabel}</button>
+      <div className="Controls-content">
+        В корзине:
+        <div className="Controls-content-count">
+          {totalItems}{' '}
+          {plural(totalItems, {
+            one: 'товар',
+            few: 'товара',
+            many: 'товаров',
+          })}
+          / {totalPrice} ₽
+        </div>
+      </div>
+      <button onClick={() => buttonHandler()}>{buttonLabel}</button>
     </div>
   );
 }
 
 Controls.propTypes = {
-  onAdd: PropTypes.func,
-  titleButton: PropTypes.node,
-  titleLabel: PropTypes.node,
+  buttonHandler: PropTypes.func,
+  totalPrice: PropTypes.number,
+  totalCount: PropTypes.number,
+  buttonLabel: PropTypes.node,
 };
 
 export default React.memo(Controls);
