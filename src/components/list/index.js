2ab44d9789ffec836107d8e960f705af951268c9
Lev Enders <leveders@mail.ru>
2024-09-19 21:28:44 +0300

    feat: update list component

--- src/components/list/index.js
+++ src/components/list/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import React from 'react';
 import Item from '../item';
 import './style.css';
 
-function List({ list, onDeleteItem, onSelectItem }) {
+function List({ list, buttonHandler, buttonLabel }) {
   return (
     <div className="List">
       {list.map(item => (
         <div key={item.code} className="List-item">
-          <Item item={item} onDelete={onDeleteItem} onSelect={onSelectItem} />
+          <Item item={item} buttonHandler={buttonHandler} buttonLabel={buttonLabel} />
         </div>
       ))}
     </div>
@@ -21,13 +21,8 @@ List.propTypes = {
       code: PropTypes.number,
     }),
   ).isRequired,
-  onDeleteItem: PropTypes.func,
-  onSelectItem: PropTypes.func,
-};
-
-List.defaultProps = {
-  onDeleteItem: () => {},
-  onSelectItem: () => {},
+  buttonHandler: PropTypes.func.isRequired,
+  buttonLabel: PropTypes.node.isRequired,
 };
 
 export default React.memo(List);
