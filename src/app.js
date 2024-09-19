31beb824f62bbaca3242173f48cce7e612eea0a7
Lev Enders <leveders@mail.ru>
2024-09-19 22:17:30 +0300

    feat: update app component

--- src/app.js
+++ src/app.js
@@ -1,46 +1,60 @@
-import React, { useCallback } from 'react';
-import List from './components/list';
+import React, { useCallback, useState } from 'react';
+import CartModal from './components/cart-modal';
 import Controls from './components/controls';
 import Head from './components/head';
+import List from './components/list';
 import PageLayout from './components/page-layout';
-
 /**
  * Приложение
  * @param store {Store} Хранилище состояния приложения
  * @returns {React.ReactElement}
  */
 function App({ store }) {
+  const [isCartOpen, setIsCartOpen] = useState(false);
+
+  const cart = store.getState().cart;
   const list = store.getState().list;
 
   const callbacks = {
-    onDeleteItem: useCallback(
+    addToCart: useCallback(
       code => {
-        store.deleteItem(code);
+        store.addToCart(code);
       },
       [store],
     ),
 
-    onSelectItem: useCallback(
+    removeFromCart: useCallback(
       code => {
-        store.selectItem(code);
+        store.removeFromCart(code);
       },
       [store],
     ),
 
-    onAddItem: useCallback(() => {
-      store.addItem();
-    }, [store]),
+    toggleCartVisible: useCallback(() => {
+      setIsCartOpen(state => !state);
+    }, []),
   };
+
+  const totalItems = cart.reduce((acc, item) => acc + item.quantity, 0);
+  const totalPrice = cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
 
   return (
     <PageLayout>
-      <Head title="Приложение на чистом JS" />
-      <Controls onAdd={callbacks.onAddItem} />
-      <List
-        list={list}
-        onDeleteItem={callbacks.onDeleteItem}
-        onSelectItem={callbacks.onSelectItem}
+      <Head title="Магазин" />
+      <Controls
+        buttonLabel={'Перейти'}
+        totalItems={totalItems}
+        totalPrice={totalPrice}
+        buttonHandler={callbacks.toggleCartVisible}
       />
+      <List list={list} buttonLabel={'Добавить'} buttonHandler={callbacks.addToCart} />
+      {isCartOpen && (
+        <CartModal
+          cart={cart}
+          removeFromCart={callbacks.removeFromCart}
+          closeModal={callbacks.toggleCartVisible}
+        />
+      )}
     </PageLayout>
   );
 }
