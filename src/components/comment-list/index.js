cc68c1580ad6293d2be95b3a86ca83f286ed51e1
Lev Enders <leveders@mail.ru>
2024-10-10 20:33:03 +0300

    feat: added comments list component

---
+++ src/components/comment-list/index.js
@@ -0,0 +1,40 @@
+import { cn as bem } from '@bem-react/classname';
+import PropTypes from 'prop-types';
+import { memo } from 'react';
+import Comment from '../comment';
+import './style.css';
+
+function CommentList({ comments, onAdd, isOpenComment, onCancel, onOpen, t, isChildList, isAuth }) {
+  const cn = bem('CommentList');
+
+  return (
+    <ul className={cn({ child: isChildList })}>
+      {comments?.map(commentItem => (
+        <li key={commentItem._id}>
+          <Comment
+            isOpenComment={isOpenComment}
+            comment={commentItem}
+            onAdd={onAdd}
+            onCancel={onCancel}
+            onOpen={onOpen}
+            isAuth={isAuth}
+            t={t}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+CommentList.propTypes = {
+  isOpenComment: PropTypes.string,
+  comments: PropTypes.array,
+  onAdd: PropTypes.func,
+  onCancel: PropTypes.func,
+  onOpen: PropTypes.func,
+  isChildList: PropTypes.bool,
+  isAuth: PropTypes.bool,
+  t: PropTypes.func,
+};
+
+export default memo(CommentList);
