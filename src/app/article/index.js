902f1bf9a4b099138c0dbebf50cb888aa734657e
Lev Enders <leveders@mail.ru>
2024-10-10 20:34:49 +0300

    feat: added new comment components to article page

--- src/app/article/index.js
+++ src/app/article/index.js
@@ -1,45 +1,57 @@
-import { memo, useCallback, useMemo } from 'react';
+import { memo, useCallback } from 'react';
+import { useDispatch, useSelector as useSelectorRedux } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import shallowequal from 'shallowequal';
+import ArticleCard from '../../components/article-card';
+import Head from '../../components/head';
+import PageLayout from '../../components/page-layout';
+import Spinner from '../../components/spinner';
+import CommentsZone from '../../containers/comments-zone';
+import LocaleSelect from '../../containers/locale-select';
+import Navigation from '../../containers/navigation';
+import TopHead from '../../containers/top-head';
+import useInit from '../../hooks/use-init';
+import useSelector from '../../hooks/use-selector';
 import useStore from '../../hooks/use-store';
 import useTranslate from '../../hooks/use-translate';
-import useInit from '../../hooks/use-init';
-import PageLayout from '../../components/page-layout';
-import Head from '../../components/head';
-import Navigation from '../../containers/navigation';
-import Spinner from '../../components/spinner';
-import ArticleCard from '../../components/article-card';
-import LocaleSelect from '../../containers/locale-select';
-import TopHead from '../../containers/top-head';
-import { useDispatch, useSelector } from 'react-redux';
-import shallowequal from 'shallowequal';
 import articleActions from '../../store-redux/article/actions';
+import commentsActions from '../../store-redux/comments/actions';
 
 function Article() {
   const store = useStore();
 
   const dispatch = useDispatch();
-  // Параметры из пути /articles/:id
 
   const params = useParams();
+  const { t } = useTranslate();
 
   useInit(() => {
-    //store.actions.article.load(params.id);
     dispatch(articleActions.load(params.id));
+    dispatch(commentsActions.load(params.id));
   }, [params.id]);
 
-  const select = useSelector(
+  const select = useSelectorRedux(
     state => ({
       article: state.article.data,
       waiting: state.article.waiting,
+      comments: state.comments.items,
+      commentsCount: state.comments.count,
+      commentsWaiting: state.comments.waiting,
     }),
     shallowequal,
   ); // Нужно указать функцию для сравнения свойства объекта, так как хуком вернули объект
 
-  const { t } = useTranslate();
+  const { isAuth, userName } = useSelector(state => ({
+    isAuth: state.session.exists,
+    userName: state.session.user?.profile?.name,
+  }));
 
   const callbacks = {
-    // Добавление в корзину
     addToBasket: useCallback(_id => store.actions.basket.addToBasket(_id), [store]),
+    addComment: useCallback(
+      (data, onSuccess) => dispatch(commentsActions.add(data, userName, onSuccess)),
+      [userName],
+    ),
   };
 
   return (
@@ -51,6 +63,16 @@ function Article() {
       <Navigation />
       <Spinner active={select.waiting}>
         <ArticleCard article={select.article} onAdd={callbacks.addToBasket} t={t} />
+      </Spinner>
+      <Spinner active={select.commentsWaiting}>
+        <CommentsZone
+          comments={select.comments}
+          count={select.commentsCount}
+          articleId={params.id}
+          onAdd={callbacks.addComment}
+          isAuth={isAuth}
+          t={t}
+        />
       </Spinner>
     </PageLayout>
   );
