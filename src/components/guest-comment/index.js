6ad415a4c30863efe671e333b47f2b43934c9163
Lev Enders <leveders@mail.ru>
2024-10-13 20:45:38 +0300

    fix: added back state

--- src/components/guest-comment/index.js
+++ src/components/guest-comment/index.js
@@ -1,17 +1,19 @@
 import { cn as bem } from '@bem-react/classname';
 import PropTypes from 'prop-types';
 import { memo } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './style.css';
 
 function GuestComment({ text, onCancel, buttonText, t }) {
   const cn = bem('GuestComment');
 
+  const location = useLocation();
+
   const hasButton = Boolean(buttonText);
 
   return (
     <div className={cn()}>
-      <Link className={cn('link')} to="/login">
+      <Link className={cn('link')} to="/login" state={{ back: location.pathname }}>
         {t('comments.login')}
       </Link>
       <span>{text}</span>
